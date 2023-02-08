522f44398144e3ce37b5739b711b699f0ffc823c
Pedro Henrique <pcorreia@paranoa.com.br>
2023-02-08 13:01:33 -0300

    Formulário RC

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,54 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+require '../forms/PHPMailerAutoload.php';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$name = $_POST["name"];
+$email = $_POST["email"];
+$telefone = $_POST["telefone"];
+$message = $_POST["message"];
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$mail = new PHPMailer;
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+$mail->isSMTP();                        // Setar o uso do SMTP
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+$mail->Host = 'email-ssl.com.br';      // Servidor smtp 
 
-  echo $contact->send();
-?>
+$mail->SMTPAuth = true;                 // Habilita a autenticação do form
+
+$mail->Username = 'henrique.adm@..............com.br';       // Conta de e-mail que realizará o envio
+
+$mail->Password = '...........';       // Senha da conta de e-mail
+
+$mail->Port = 587;                       // Porta de conexão 
+
+$mail->From = 'henrique.adm@............com.br';             // e-mail From deve ser o mesmo de "username" (contadeEmail)
+
+$mail->FromName = 'Contato Site';                 // Nome que será exibido ao receber a mensagem. 
+
+$mail->addAddress('contato@............com.br', 'Contato'); // Destinatário 
+
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+
+$mail->Subject = 'Formulario de Contato do Site';  //Assunto da Mensagem
+
+$mail->Body    =  "<p>Contato Site</p>"
+                . "<p><b>Nome: </b> $name\n</p>"
+                . "<p><b>Email: </b> $email\n</p>"
+                . "<p><b>Telefone: </b> $telefone\n</p>"
+                . "<b>Mensagem: </b> $message\n"; // Corpo da mensagem
+
+//$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+
+
+if (!$mail->send()) {
+
+    echo 'Message could not be sent.';
+
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+
+    header("Location: https://www.rokalucoberturas.com.br/obrigado.html");
+}
