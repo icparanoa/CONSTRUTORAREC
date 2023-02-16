56335ffa2619c435d94b2e75a91f700b1ed56f49
Pedro Henrique <pcorreia@paranoa.com.br>
2023-02-16 16:21:22 -0300

    Versão final

--- forms/contact.php
+++ forms/contact.php
@@ -3,8 +3,10 @@
 require '../forms/PHPMailerAutoload.php';
 
 $name = $_POST["name"];
-$email = $_POST["email"];
 $telefone = $_POST["telefone"];
+$email = $_POST["email"];
+$obra = $_POST["obra"];
+$servico = $_POST["servico"];
 $message = $_POST["message"];
 
 $mail = new PHPMailer;
@@ -15,17 +17,17 @@ $mail->Host = 'email-ssl.com.br';      // Servidor smtp
 
 $mail->SMTPAuth = true;                 // Habilita a autenticação do form
 
-$mail->Username = 'henrique.adm@..............com.br';       // Conta de e-mail que realizará o envio
+$mail->Username = 'adeilson@construtorarec.com.br';       // Conta de e-mail que realizará o envio
 
-$mail->Password = '...........';       // Senha da conta de e-mail
+$mail->Password = 'R3cC0nstrutor@';       // Senha da conta de e-mail
 
 $mail->Port = 587;                       // Porta de conexão 
 
-$mail->From = 'henrique.adm@............com.br';             // e-mail From deve ser o mesmo de "username" (contadeEmail)
+$mail->From = 'adeilson@construtorarec.com.br';             // e-mail From deve ser o mesmo de "username" (contadeEmail)
 
 $mail->FromName = 'Contato Site';                 // Nome que será exibido ao receber a mensagem. 
 
-$mail->addAddress('contato@............com.br', 'Contato'); // Destinatário 
+$mail->addAddress('contato@construtorarec.com.br', 'Contato'); // Destinatário 
 
 
 $mail->isHTML(true);                                  // Set email format to HTML
@@ -36,8 +38,10 @@ $mail->Subject = 'Formulario de Contato do Site';  //Assunto da Mensagem
 $mail->Body    =  "<p>Contato Site</p>"
                 . "<p><b>Nome: </b> $name\n</p>"
                 . "<p><b>Email: </b> $email\n</p>"
-                . "<p><b>Telefone: </b> $telefone\n</p>"
-                . "<b>Mensagem: </b> $message\n"; // Corpo da mensagem
+                . "<p><b>Telefone: </b> $obra\n</p>"
+                . "<p><b>Endereço da Obra: </b> $telefone\n</p>"
+                . "<p><b>Descrição do Serviço: </b> $servico\n</p>"
+                . "<b>Mensagem: </b> $message\n";
 
 //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
@@ -50,5 +54,5 @@ if (!$mail->send()) {
     echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
 
-    header("Location: https://www.rokalucoberturas.com.br/obrigado.html");
+    header("Location: https://www.construtorarec.com.br/obrigado.html");
 }
